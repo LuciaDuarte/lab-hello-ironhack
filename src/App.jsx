de71927dd3dca72f324b8b2bf9b22a1e1c552104
Lucia Duarte <analuciacfduarte@gmail.com>
2020-08-10 14:40:16 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
 
 import './App.css';
+
+function Declarative() {
+  return (
+    <article>
+      <img src="./images/icon1.png" alt="declarative-icon" />
+      <h2>Declarative</h2>
+      <p>React makes it painless to create interactive UIs.</p>
+    </article>
+  );
+}
+function Components() {
+  return (
+    <article>
+      <img src="./images/icon1.png" alt="declarative-icon" />
+      <h2>Components</h2>
+      <p>RBuild encapsulated components that manage their state.</p>
+    </article>
+  );
+}
+function SingleWay() {
+  return (
+    <article>
+      <img src="./images/icon1.png" alt="declarative-icon" />
+      <h2>Single-Way</h2>
+      <p>A set of immutable values are passed to the component's.</p>
+    </article>
+  );
+}
+function ElementJSX() {
+  return (
+    <article>
+      <img src="./images/icon1.png" alt="declarative-icon" />
+      <h2>JSX</h2>
+      <p>Statically-typed, designed to run on modern browsers.</p>
+    </article>
+  );
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <div className="Header-text">
+            <h1>Say hello to ReactJS</h1>
+            <p>
+              You will learn a Frontend framework from scratch, to become an
+              Ninka Developer.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Awesome!
+            </a>
+          </div>
         </header>
+        <div className="Features">
+          <Declarative />
+          <Components />
+          <SingleWay />
+          <ElementJSX />
+        </div>
       </div>
     );
   }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav>
+      <img src="./images/ironhack-logo.svg" alt="ironhack-logo" />
+      <img src="./images/menu-top.svg" alt="menu-logo" />
+    </nav>
+  );
+}
+
+export default Navbar;
